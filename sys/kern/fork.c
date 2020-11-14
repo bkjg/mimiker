39310dc501f0d70877d567b1fbe966c14dc792d5
Barbara Kaczorowska <bkjg252@gmail.com>
2020-11-14 16:00:49 +0100

    add sharing memory in case when this memory is anonymous

--- sys/kern/fork.c
+++ sys/kern/fork.c
@@ -65,7 +65,7 @@ int do_fork(void (*start)(void *), void *arg, pid_t *cldpidp, int flags) {
     cred_fork(child, parent);
   }
 
-  if ((flags & VM_SHARED) != 0) {
+  if ((flags & VM_SHARED) != 0 || (flags & VM_ANON) != 0 ) {
     /* Share the parent's entire process memory with child and increment reference counter for vm_map_t struture */
     child->p_uspace = vm_map_share(parent->p_uspace);
   } else {
